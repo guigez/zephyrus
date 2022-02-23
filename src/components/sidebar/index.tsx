5869407ccbc3964ff7d227d5db4bfbd125fcf068
Guilherme <70561321+YuutaDragon@users.noreply.github.com>
2022-02-22 23:15:47 -0300

    refactor  - sidebar

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,45 +1,43 @@
-import Image from 'next/image';
-
-import title from '../../../public/Title.svg'
-
 import { RiDashboardFill } from 'react-icons/ri'
 import { BsBoxSeam } from 'react-icons/bs'
 import { BsBoxArrowLeft } from 'react-icons/bs'
 
 import styles from './styles.module.scss'
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 
 export function Sidebar() {
+  const { asPath } = useRouter()
+
+  console.log(asPath)
+
   return (
-    <aside className={styles.aside}>
-      <div>
-        <div className={styles.title}>
-          <Image src={title} alt="Logo Zephyrus" />
+    <aside className={styles.asideContainer}>
+      <div className={styles.asideContent}>
+        <img className={styles.title} src="/Title.svg" alt="Title of Application" />
+        <nav className={styles.nav}>
+          <Link href="/dashboard">
+            <a className={asPath === '/dashboard' ? styles.active + ' ' + styles.item : styles.item}>
+              <RiDashboardFill className={styles.icon} />
+              Dashboard
+            </a>
+          </Link>
+          <Link href="/deliveries">
+            <a className={asPath === '/deliveries' ? styles.active + ' ' + styles.item : styles.item}>
+              <BsBoxSeam className={styles.icon} />
+              Entregas
+            </a>
+          </Link>
+        </nav>
+        <div className={styles.footer}>
+          <Link href="/">
+            <a className={styles.footerItem}>
+              <BsBoxArrowLeft className={styles.icon} />
+              Log Out
+            </a>
+          </Link>
         </div>
-        <div className={styles.menu}>
-          <ul>
-            <li>
-              <a className={styles.item} href="dashboard">
-                <RiDashboardFill className={styles.icon} />
-                <div className={styles.titleMenu}>Dashboard</div>
-              </a>
-            </li>
-            <li>
-              <a className={styles.item} href="deliveries">
-                <BsBoxSeam className={styles.icon} />
-                <div className={styles.titleMenu}>Entregas</div>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      <div className={styles.footer}>
-        <Link href="/">
-          <a className={styles.footerItem}>
-            <BsBoxArrowLeft className={styles.icon} />
-            <div className={styles.titleMenu}>Log Out</div>
-          </a>
-        </Link>
       </div>
     </aside>
   );
