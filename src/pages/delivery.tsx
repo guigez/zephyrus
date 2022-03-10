b5c3f8c322dd872757794985b966aacbcd0a412b
Matheus Santana <matheus.santana6415@gmail.com>
2022-03-10 19:33:30 -0300

    update delivery
    Adicionado modal para sugerir preço

--- src/pages/delivery.tsx
+++ src/pages/delivery.tsx
@@ -2,11 +2,15 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { Header } from '../components/header';
 import { Sidebar } from '../components/sidebar';
-import Modal from '../components/modal/modal.js';
+import React, { useState } from "react";
+import Modal from 'react-modal';
 
 import styles from '../styles/delivery.module.scss'
 
 const Delivery: NextPage = () => {
+  const[modalIsOpen, setModalIsOpen] = useState(false)
+  const[preco, setPreco] = useState('Nenhum');
+
   return (
     <>
       <Head>
@@ -85,9 +89,61 @@ const Delivery: NextPage = () => {
                   </tr>
                 </tbody>
 
-                <button /*onClick={}*/>
-                  Sugerir
-                </button>
+                <button onClick={() => setModalIsOpen(true)} style={{marginTop: '3rem'}}>Sugerir Preço</button>
+                <Modal 
+                  isOpen={modalIsOpen} 
+                  shouldCloseOnOverlayClick={true} 
+                  onRequestClose={() => setModalIsOpen(false)}
+                  style={
+                    {
+                      content: {
+                        borderRadius: '8px',
+                        width: '400px',
+                        height: '300px',
+                        marginLeft: '40%',
+                        color: 'blue',
+                      }
+                    }
+                  }
+                >
+                    <label style={{marginLeft: '20%'}}>
+                      Sugira um preço para a entrega:
+                      <p></p>
+                      <input type='number' name="preco" placeholder="Preço sugerido" onChange={event => setPreco(event.target.value)} style={{marginTop: '1rem', marginLeft: '29%', width: '41%'}}/>
+                    </label>
+                    <p></p>
+                    <input type="submit" value="Salvar" onClick={() => setModalIsOpen(false)}
+                    style={
+                      {
+                        backgroundColor: 'cornflowerblue',
+                        color: 'white',
+                        borderRadius: '4px',
+                        fontSize: '20px',
+                        border: '2px solid blue',
+                        padding: '14px 40px',
+                        textAlign: 'center',
+                        marginLeft: '30%',
+                        marginTop: '1rem',
+                      }
+                    }
+                    />
+                    <p></p>
+                  <button onClick={() => setModalIsOpen(false)}
+                  style={
+                    {
+                      backgroundColor: 'cornflowerblue',
+                      color: 'white',
+                      borderRadius: '4px',
+                      fontSize: '20px',
+                      border: '2px solid blue',
+                      padding: '14px 40px',
+                      textAlign: 'center',
+                      marginLeft: '27%',
+                      marginTop: '1rem',
+                    }
+                  }>Cancelar</button>
+                </Modal>
+                <p style={{marginTop: '1rem', fontSize: '20px'}}>Preço sugerido: R${preco}</p>
               </table>
             </div>
           </div>
